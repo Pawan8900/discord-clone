86dcd410c5747894b7a8c1b52c5c63cc7d0c543b
Pawan <pawanacharya101@gmail.com>
2023-10-31 15:37:34 +0545

    Chat messages component done

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,6 +1,19 @@
 "use client"
 
-import { Member } from "@prisma/client";
+
+import { Loader2, ServerCrash } from "lucide-react";
+
+import { useChatQuery } from "@/hooks/use-chat-query";
+import { Member, Message, Profile } from "@prisma/client";
+import { Fragment } from "react";
+
+
+
+type MessageWithMemberWithProfile = Message & {
+    member: Member & {
+        profile: Profile
+    }
+}
 
 
 interface ChatMessagesProps {
@@ -27,7 +40,57 @@ export const ChatMessages = ({
     paramValue,
     type,
 }: ChatMessagesProps) => {
+    const queryKey = `chat:${chatId}`
+    const {
+        data,
+        fetchNextPage,
+        hasNextPage,
+        isFetchingNextPage,
+        status
+    } = useChatQuery({
+        queryKey,
+        apiUrl,
+        paramKey,
+        paramValue
+    })
+
+
+    if (status === "pending") {
+        return (
+            <div className="flex flex-col flex-1 justify-center items-center">
+                <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+                <p className="text-xs text-zinc-500 dark:text-zinc-400">
+                    Loading messages...
+                </p>
+            </div>
+        )
+    }
+
+    if (status === "error") {
+        return (
+            <div className="flex flex-col flex-1 justify-center items-center">
+                <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
+                <p className="text-xs text-zinc-500 dark:text-zinc-400">
+                    Something went wrong!
+                </p>
+            </div>
+        )
+    }
+
     return (
-        <div>Chat Messages</div>
+        <>
+            <div>Chat Messages</div>
+            <div className="flex flex-col-reverse mt-auto">
+                {data?.pages?.map((group, i) => (
+                    <Fragment key={i}>
+                        {group.items.map((message: MessageWithMemberWithProfile) => (
+                            <div key={message.id}>
+                                {message.content}
+                            </div>
+                        ))}
+                    </Fragment>
+                ))}
+            </div>
+        </>
     )
 }
